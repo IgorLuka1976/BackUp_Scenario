e20d3a049bfa29282b24ee5d5f92556e36f33fa7
Igor Luca <89498870+IgorLuka1976@users.noreply.github.com>
2023-03-11 10:26:24 +0200

    Update DifferentialBackup.sql

--- BackUp_Scenario/DifferentialBackup.sql
+++ BackUp_Scenario/DifferentialBackup.sql
@@ -5,11 +5,11 @@
 USE AdventureWorksLT2019
 GO
 
-----backup Folder D:\backuptest
+----backup Folder D:\backuptestDif
 
 ------Differential backup with Compress option and append media
 
 DECLARE @pathBackup  NVARCHAR(100)
-SET @pathBackup =  N'D:\backuptest\Dif_AdventureWorksLT2019_' + REPLACE(REPLACE(CONVERT(VARCHAR,GETDATE(), 120),':','_'),' ','_') + '.bak'
+SET @pathBackup =  N'D:\backuptestDif\Dif_AdventureWorksLT2019_' + REPLACE(REPLACE(CONVERT(VARCHAR,GETDATE(), 120),':','_'),' ','_') + '.bak'
 BACKUP DATABASE [crm] TO  DISK = @pathBackup WITH  DIFFERENTIAL ,  DESCRIPTION = N'Differential Backup', NOFORMAT
 , NOINIT,  NAME = N'AdventureWorksLT2019-Differential Database Backup', SKIP, NOREWIND, NOUNLOAD, COMPRESSION,  STATS = 10
